2726c1939ef6a79bb101ee9f42b3fba654b83387
mdiakhate <mdiakhate@revadmins-MacBook-Pro.local>
2024-09-10 18:47:31 +0200

    Change App bars to have the same on each page

--- lib/components/privacy_policy_page.dart
+++ lib/components/privacy_policy_page.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:mfk_guinee_transport/components/base_app_bar.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
   const PrivacyPolicyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Politique de confidentialité'),
-        backgroundColor: Colors.green,
-      ),
-      body: const Center(
+    return const Scaffold(
+      appBar: BaseAppBar(title: 'Politique de confidentialité'),
+      body: Center(
         child: Text('Page Politique de confidentialité'),
       ),
     );
