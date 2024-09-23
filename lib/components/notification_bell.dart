a0210c0861effb88ae9dc86a9317ee868f8e2c3c
MDiakhate12 <46793415+MDiakhate12@users.noreply.github.com>
2024-09-23 23:19:22 +0200

    Feature/global app refactoring (#6)
    * Change App bars to have the same on each page
    
    * Home page glitch fix
    
    * Update Base app bar's notification bell
    
    ---------
    
    Co-authored-by: mdiakhate <mdiakhate@revadmins-MacBook-Pro.local>

--- lib/components/notification_bell.dart
+++ lib/components/notification_bell.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mfk_guinee_transport/components/notifications_page.dart';
+import 'package:mfk_guinee_transport/helper/constants/colors.dart';
 
 class NotificationBell extends StatelessWidget {
   const NotificationBell({
@@ -34,9 +35,28 @@ class NotificationBell extends StatelessWidget {
               color: Colors.red,
               shape: BoxShape.circle,
             ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const NotificationsPage()),
+              );
+            },
           ),
-        )
-      ],
+          Positioned(
+            top: 12,
+            right: 12,
+            child: Container(
+              height: 12,
+              width: 12,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
